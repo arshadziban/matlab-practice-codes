88d3afc56558f8cb72e1a5ddbe2c0ead05022194
arsahdziban <arshadziban031201@gmail.com>
2025-09-06 21:42:40 +0600

    updated

--- use_linspace.m
+++ use_linspace.m
@@ -1,2 +1,2 @@
-v = linspace(1,5,9)
-colVec = v(:)
+v = linspace(1,5,9);   % Create a row vector of 9 equally spaced points between 1 and 5
+colVec = v(:);         % Convert the row vector 'v' into a column vector
